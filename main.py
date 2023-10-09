a96419015944c6d0c2490e64e8e842fbe7c2573f
Sanskar Mishra <mishrasanskar60@gmail.com>
2023-10-10 02:15:07 +0530

    added some files and a main.py file

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+from flask_mysqldb import MySQL
+import mysql.connector
+
+app = Flask(__name__)
+app.secret_key= 'my_new_key'
+
+app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+://root:@localhost:3306/ftm'
+mysql = MySQL(app)
+
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM applicants")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', applicants=data)
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == "POST":
+        # flash("Data Inserted Successfully")
+        name = request.form['name']
+        age = request.form['age']
+        email_id = request.form['email']
+        phone_no = request.form['phone']
+        cgpa = request.form['cgpa']
+        college = request.form['college']
+        skill =request.form['skill']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO applicants (name, age, email_id, phone_no, cgpa, skill,college) VALUES (%s, %s,%s,%s, %s,%s,%s)", (name,age, email_id, phone_no,cgpa,skill, college))
+        cur.execute("SELECT * FROM applicants ORDER BY id DESC LIMIT 1")
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+@app.route('/delete/<string:id_data>', methods = ['GET'])
+def delete(id_data):
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM applicants WHERE id=%s", (id_data,))
+    mysql.connection.commit()
+    return redirect(url_for('index'))
+
+
+
+@app.route('/update', methods= ['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        email_id = request.form['email']
+        phone_no = request.form['phone']
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+        UPDATE applicants SET name=%s, email_id=%s, phone_no=%s
+        WHERE id=%s
+        """, (name, email_id, phone_no, id_data))
+        flash("Data Updated Successfully")
+        return redirect(url_for('index'))
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
